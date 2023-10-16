9d9a617293d92972472b48b4044b8620c3052c09
semihartan <s-785412369@hotmail.com>
2023-10-16 16:38:13 +0300

    Initial commit.

---
+++ src/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define GET_BIT(x, i) (((x) >> (i)) & 1)
+#define SET_BIT(x, i, b) ((x) |= (b << (i)))
+
+
+bool overflow_flag = false;
+
+void full_bit_adder(uint32_t a, uint32_t b, uint32_t cin, uint32_t *s, uint32_t *cout)
+{
+	*s = (a ^ b) ^ cin;
+	*cout = (a & b) | (cin & (a ^ b));
+}
+
+uint32_t binary_adder(uint32_t a, uint32_t b, uint32_t operand_size)
+{
+	int i = 0;
+	uint32_t cin = 0;
+	uint32_t cout = 0; 
+	uint32_t s = 0;
+	uint32_t sum = 0;
+	overflow_flag = false;
+	while(i < operand_size)
+	{
+		uint32_t bit_a = GET_BIT(a, i);
+		uint32_t bit_b = GET_BIT(b, i);
+		cin = cout;
+		full_bit_adder(bit_a, bit_b, cin, &s, &cout);
+		SET_BIT(sum, i, s);
+		i++;
+	} 
+	if(cout)
+		overflow_flag = true;
+	return sum;
+}
+
+uint32_t nible_adder(uint32_t a, uint32_t b)
+{
+	return binary_adder(a, b, 4);
+}
+uint32_t byte_adder(uint32_t a, uint32_t b)
+{
+	return binary_adder(a, b, 8);
+}
+uint32_t word_adder(uint32_t a, uint32_t b)
+{
+	return binary_adder(a, b, 16);
+}
+uint32_t dword_adder(uint32_t a, uint32_t b)
+{
+	return binary_adder(a, b, 32);
+}
+
+
+
+int main()
+{
+	printf("%d + %d = %d, overflow_flag = %d\n", 2, 3, nible_adder(2, 3), overflow_flag);
+	printf("%d + %d = %d, overflow_flag = %d\n", 2, 254, byte_adder(2, 254), overflow_flag);
+	printf("%d + %d = %d, overflow_flag = %d\n", 2, 254, word_adder(2, 254), overflow_flag);
+	return 0;
+}
